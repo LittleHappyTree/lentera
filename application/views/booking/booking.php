377b4e828a1ae7f63b361ea6bc89ed4cf70683d8
Wayan Kantor <wayanaditya27@yahoo.com>
2020-02-11 18:53:40 +0700

    add google maps api

--- application/views/booking/booking.php
+++ application/views/booking/booking.php
@@ -50,6 +50,20 @@
                 <div class="validation"></div>
               </div>
             </div>
+            <div class="form-row">
+              <label for="staticEmail" class="col-sm-4 col-form-label text-right">Pickup Location</label>
+              <div class="form-group col-md-6">
+                <input type="text" name="pickup" id="pac-input" class="form-control" style="background: #fff" required />
+                <span id="alert-date" style="color:#f82249"><small id="alert-text"></small></span>
+                <div class="validation"></div>
+              </div>
+            </div>
+            <div class="form-row">
+              <label for="staticEmail" class="col-sm-4 col-form-label text-right">&nbsp;</label>
+              <div class="form-group col-md-6">
+                <div id="map"></div>
+              </div>
+            </div>
             <div class="text-center"><button type="submit" id="submit-book">Send Message</button></div>
           </form>
         </div>
@@ -57,3 +71,73 @@
       </div>
     </section><!-- #booking -->
 </main>
+<script>
+function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: -33.8688,
+            lng: 151.2195
+        },
+        zoom: 13,
+        mapTypeId: 'roadmap'
+    });
+
+    // Membuat Kotak pencarian terhubung dengan tampilan map
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Mengaktifkan detail pada suatu tempat ketika pengguna
+    // memilih salah satu dari daftar prediksi tempat 
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // menghilangkan marker tempat sebelumnya
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // Untuk setiap tempat, dapatkan icon, nama dan tempat.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Membuat Marker untuk setiap tempat
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location
+            }));
+
+            if (place.geometry.viewport) {
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+}
+</script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDjwUmT_Cm1yaP_b2AaDdMy-TS4AhqqLlY&libraries=places&callback=initAutocomplete"></script>
