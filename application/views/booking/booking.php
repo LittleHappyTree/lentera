5d69b43ef4c3df11aaee2301749cf3c5454e4dca
Wayan Kantor <wayanaditya27@yahoo.com>
2020-02-12 14:18:34 +0700

    change coordinate init

--- application/views/booking/booking.php
+++ application/views/booking/booking.php
@@ -75,8 +75,8 @@
 function initAutocomplete() {
     var map = new google.maps.Map(document.getElementById('map'), {
         center: {
-            lat: -33.8688,
-            lng: 151.2195
+            lat: -6.194618,
+            lng: 106.840006
         },
         zoom: 13,
         mapTypeId: 'roadmap'
